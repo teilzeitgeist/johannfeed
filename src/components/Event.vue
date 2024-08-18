d2fce82ce81e603baddf2893d66bda52106f5523
Dorbsky <christopher.tempel@gmail.com>
2024-08-18 13:36:45 +0200

    Animation gefixt und leichte Stil-Anspassung

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,66 +1,66 @@
 <template>
-  <div class="event" :class="{ 'event--longer': isLongEvent }">
-    <div class="event__date" :class="getCategoryColor()">
-      <span class="day">{{ day }}</span>
-      <span class="month">{{ month }}</span>
+    <div class="event" :class="{ 'event--longer': isLongEvent }">
+        <div class="event__date" :class="getCategoryColor()">
+            <span class="day">{{ day }}</span>
+            <span class="month">{{ month }}</span>
+        </div>
+        <div class="event__infos">
+            <div class="event__title">
+                {{ event.title }}
+            </div>
+            <div class="event__time">
+                {{ parseTime() }}
+            </div>
+            <div class="event__place">
+                {{ parsePlace() }}<br/>
+                {{ parseStreet() }}
+                <!--{{ parseCategories() }}-->
+            </div>
+        </div>
     </div>
-    <div class="event__infos">
-      <div class="event__title">
-        {{ event.title }}
-      </div>
-      <div class="event__time">
-        {{ parseTime() }}
-      </div>
-      <div class="event__place">
-        {{ parsePlace() }}<br />
-        {{ parseStreet() }}
-        <!--{{ parseCategories() }}-->
-      </div>
-    </div>
-  </div>
-  <!--<img :src="image.img.src" v-if="isImage && image" :alt="image.img.alt"/>-->
+    <!--<img :src="image.img.src" v-if="isImage && image" :alt="image.img.alt"/>-->
 </template>
 
 <script lang="ts" setup>
-import { XMLParser } from "fast-xml-parser";
+import {XMLParser} from "fast-xml-parser";
 import dayjs from "dayjs";
-import { ref } from "vue";
+import {ref} from "vue";
 
 const props = defineProps(["event"]);
 
 const categoryColors = [
-  {
-    category: "begegnung",
-    color: "red",
-  },
-  {
-    category: "nachbarschaftshilfe",
-    color: "red",
-  },
-  {
-    category: "kurs",
-    color: "blue",
-  },
-  {
-    category: "beratung",
-    color: "blue",
-  },
-  {
-    category: "sport-spiel",
-    color: "yellow",
-  },
-  {
-    category: "jugendangebot",
-    color: "yellow",
-  },
-  {
-    category: "kinderangebot",
-    color: "yellow",
-  },
-  {
-    category: "stadtteilspaziergang",
-    color: "green",
-  },
+    {
+        category: "begegnung",
+        color: "red",
+    },
+    {
+        category: "nachbarschaftshilfe",
+        color: "red",
+    },
+    {
+        category: "kurs",
+        color: "blue",
+    },
+    {
+        category: "beratung",
+        color: "blue",
+    },
+    {
+        category: "sport-spiel",
+        color: "yellow",
+    },
+    {
+        category: "jugendangebot",
+        color: "yellow",
+    },
+    {
+        category: "kinderangebot",
+        color: "yellow",
+    },
+    {
+        category: "stadtteilspaziergang",
+        color: "green",
+    },
 ];
 
 let meta = props.event.description.split("<br />");
@@ -74,48 +74,48 @@ let month = ref(dayjs(props.event.pubDate).format("MMM"));
 let isLongEvent = ref(false);
 
 const init = async () => {
-  if (isImage) {
-    const parsingOptions = {
-      ignoreAttributes: false,
-      attributeNamePrefix: "",
-      stopNodes: ["*.pre", "*.script"],
-      processEntities: true,
-      htmlEntities: true,
-    };
-    const parser = new XMLParser(parsingOptions);
-    image = await parser.parse(image);
-  }
+    if (isImage) {
+        const parsingOptions = {
+            ignoreAttributes: false,
+            attributeNamePrefix: "",
+            stopNodes: ["*.pre", "*.script"],
+            processEntities: true,
+            htmlEntities: true,
+        };
+        const parser = new XMLParser(parsingOptions);
+        image = await parser.parse(image);
+    }
 };
 
 const parseTime = () => {
-  let time = "";
-  const timeSplit = meta[0].split("-");
-  if (timeSplit && timeSplit.length > 1) {
-    // Überspannendes Event
-    if (timeSplit.length === 4) {
-      isLongEvent.value = true;
-      const firstDay = dayjs(timeSplit[0], ["D.MMMM YYYY"], "de");
-      const secondDay = dayjs(timeSplit[1], ["D.MMMM YYYY"], "de");
-      day.value = firstDay.date() + "–" + secondDay.date();
+    let time = "";
+    const timeSplit = meta[0].split("-");
+    if (timeSplit && timeSplit.length > 1) {
+        // Überspannendes Event
+        if (timeSplit.length === 4) {
+            isLongEvent.value = true;
+            const firstDay = dayjs(timeSplit[0], ["D.MMMM YYYY"], "de");
+            const secondDay = dayjs(timeSplit[1], ["D.MMMM YYYY"], "de");
+            day.value = firstDay.date() + "–" + secondDay.date();
 
-      if (!firstDay.isSame(secondDay, "month")) {
-        month.value = firstDay.format("MMM") + "-" + secondDay.format("MMM");
-      }
-      time = timeSplit[2] + " – " + timeSplit[3];
-    } else {
-      day.value = dayjs(props.event.pubDate).date().toString();
-      time = timeSplit[1] + " – " + timeSplit[2];
+            if (!firstDay.isSame(secondDay, "month")) {
+                month.value = firstDay.format("MMM") + "-" + secondDay.format("MMM");
+            }
+            time = timeSplit[2] + " – " + timeSplit[3];
+        } else {
+            day.value = dayjs(props.event.pubDate).date().toString();
+            time = timeSplit[1] + " – " + timeSplit[2];
+        }
     }
-  }
-  return time;
+    return time;
 };
 
 const parsePlace = () => {
-  return meta[1];
+    return meta[1];
 };
 
 const parseStreet = () => {
-  return meta[2];
+    return meta[2];
 };
 
 /*const parseImage = () => {
@@ -124,26 +124,26 @@ const parseStreet = () => {
 };*/
 
 const parseCategories = () => {
-  const parser = new DOMParser();
-  let parsedHtml = parser.parseFromString(meta[4], "text/html");
-  let items = parsedHtml.querySelectorAll("ul li a");
-  let categories = [];
-  items.forEach((item: HTMLAnchorElement) => {
-    let category = item.href.split("categories/")[1];
-    if (category && category.length > 0) {
-      category = category.split("/")[0];
-      if (category && category.length > 0 && !categories.includes(category)) {
-        categories.push(category);
-      }
-    }
-  });
-  return categories;
+    const parser = new DOMParser();
+    let parsedHtml = parser.parseFromString(meta[4], "text/html");
+    let items = parsedHtml.querySelectorAll("ul li a");
+    let categories = [];
+    items.forEach((item: HTMLAnchorElement) => {
+        let category = item.href.split("categories/")[1];
+        if (category && category.length > 0) {
+            category = category.split("/")[0];
+            if (category && category.length > 0 && !categories.includes(category)) {
+                categories.push(category);
+            }
+        }
+    });
+    return categories;
 };
 
 const getCategoryColor = () => {
-  const categories = parseCategories();
-  const cat = categoryColors.find((cat) => categories.includes(cat.category));
-  return cat ? cat.color : "";
+    const categories = parseCategories();
+    const cat = categoryColors.find((cat) => categories.includes(cat.category));
+    return cat ? cat.color : "";
 };
 
 init();
@@ -151,66 +151,71 @@ init();
 
 <style lang="scss">
 .event {
-  display: flex;
-  padding-bottom: var(--space-lg);
-  gap: var(--space-lg);
+    display: flex;
+    margin-bottom: var(--space-lg);
+    gap: var(--space-lg);
+    border: 2px solid var(--c1200);
+    border-radius: var(--space-xs);
 }
 
 .event--longer {
-  .event__date {
-    min-width: max-content;
-  }
+    .event__date {
+        min-width: max-content;
+    }
 }
 
 .event img {
-  max-height: 500px;
+    max-height: 500px;
 }
 
 .event__date {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--c2000);
-  min-width: 120px;
-  border: 2px solid var(--c1200);
-  border-radius: var(--space-xs);
-  padding: var(--space-md);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--c2000);
+    min-width: 120px;
+    border-radius: var(--space-xs) 0 0 var(--space-xs);
+    padding: var(--space-md);
 
-  .day {
-    font-size: var(--fs-xl);
-    font-weight: 700;
-  }
+    .day {
+        font-size: var(--fs-xl);
+        font-weight: 700;
+    }
 
-  .month {
-    font-size: var(--fs-lg);
-  }
+    .month {
+        font-size: var(--fs-lg);
+    }
 
-  &.red {
-    background-color: var(--c4000);
-  }
-  &.blue {
-    background-color: var(--c5000);
-  }
-  &.green {
-    background-color: var(--c7000);
-  }
-  &.yellow {
-    background-color: var(--c6000);
-  }
+    &.red {
+        background-color: var(--c4000);
+    }
+
+    &.blue {
+        background-color: var(--c5000);
+    }
+
+    &.green {
+        background-color: var(--c7000);
+    }
+
+    &.yellow {
+        background-color: var(--c6000);
+    }
 }
 
 .event__infos {
-  display: flex;
-  flex-direction: column;
-  gap: var(--space-xs);
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-xs);
+    padding: var(--space-md) 0;
 }
 
 .event__title {
-  font-weight: 700;
+    font-weight: 700;
 }
 
 .event__time {
-  font-weight: 500;
+    font-weight: 500;
 }
 </style>
--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,24 +1,25 @@
 <template>
-  <div v-if="events.length > 0" class="event-container">
-    <transition-group
-      name="list"
-      :css="false"
-      @enter="onEnter"
-      @leave="onLeave"
-    >
-      <event
-        v-for="(event, index) in visibleEvents"
-        :key="event.guid"
-        :event="event"
-        :data-index="index"
-        class="event"
-      />
-    </transition-group>
-  </div>
-  <div v-else class="empty">
-    <div class="text">Fehler beim Laden</div>
-    <div class="loading-spinner"></div>
-  </div>
+    <div v-if="events.length > 0" class="event-container">
+        <transition-group
+            name="list"
+            :css="false"
+            @before-enter="beforeEnter"
+            @enter="onEnter"
+            @leave="onLeave"
+        >
+            <event
+                v-for="(event, index) in visibleEvents"
+                :key="event.guid"
+                :event="event"
+                :data-index="index"
+                class="event"
+            />
+        </transition-group>
+    </div>
+    <div v-else class="empty">
+        <div class="text">Fehler beim Laden</div>
+        <div class="loading-spinner"></div>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -32,94 +33,92 @@ let events = ref([]);
 let visibleEvents = ref([]);
 let init = ref(true);
 
-// Initialer Index und Anzahl der Events pro Seite
 const currentIndex = ref(0);
 const eventsPerPage = ref(6);
 
 onMounted(async () => {
-  const { data } = await axios.get("http://localhost:5173/feed.xml");
-  //const { data } = await axios.get("https://www.johannstadt.de/events/feed");
+    const { data } = await axios.get("http://localhost:5173/feed.xml");
+    //const { data } = await axios.get("https://www.johannstadt.de/events/feed");
 
-  const xmlParser = new XMLParser();
-  const { rss } = xmlParser.parse(data);
+    const xmlParser = new XMLParser();
+    const { rss } = xmlParser.parse(data);
 
-  events.value = rss.channel.item;
+    events.value = rss.channel.item;
 
-  updateVisibleEvents();
-  // console.log("Events:", events.value);
-  // Intervall zum automatischen Wechseln der Seite
-  setInterval(nextPage, 10000); // Wechselt alle 10 Sekunden zur nächsten Seite
+    updateVisibleEvents();
+
+    setInterval(nextPage, 10000); // Wechselt alle 10 Sekunden zur nächsten Seite
 });
 
-// Methode zum Aktualisieren der sichtbaren Events
 const updateVisibleEvents = () => {
-  visibleEvents.value = events.value.slice(
-    currentIndex.value,
-    currentIndex.value + eventsPerPage.value,
-  );
-  if (!init.value) visibleEvents.value = visibleEvents.value.slice().reverse();
+    visibleEvents.value = events.value.slice(
+        currentIndex.value,
+        currentIndex.value + eventsPerPage.value,
+    );
+    if (!init.value) visibleEvents.value = visibleEvents.value.slice().reverse();
 };
 
-// Methode zum Wechseln der Seite
 const nextPage = () => {
-  if (currentIndex.value + eventsPerPage.value >= events.value.length) {
-    currentIndex.value = 0;
-  } else {
-    currentIndex.value += eventsPerPage.value;
-  }
-  init.value = false;
-  updateVisibleEvents();
+    if (currentIndex.value + eventsPerPage.value >= events.value.length) {
+        currentIndex.value = 0;
+    } else {
+        currentIndex.value += eventsPerPage.value;
+    }
+    init.value = false;
+    updateVisibleEvents();
+};
+
+const beforeEnter = (el) => {
+    el.style.opacity = 0;
+    el.style.transform = "translateX(300px)";
 };
 
 const onEnter = (el, done) => {
-  anime({
-    targets: el,
-    opacity: [0, 1],
-    translateX: [300, 0],
-    duration: 1000,
-    easing: "easeOutElastic",
-    delay: el.dataset.index * 100, // build a custom delay based on the index
-    complete: done,
-  });
+    anime({
+        targets: el,
+        opacity: [0, 1],
+        translateX: [300, 0],
+        duration: 1000,
+        easing: "easeOutElastic",
+        delay: el.dataset.index * 100,
+        complete: done,
+    });
 };
 
-// called when the leave transition starts.
-// use this to start the leaving animation.
 const onLeave = (el, done) => {
-  anime({
-    targets: el,
-    opacity: [1, 0],
-    translateY: [0, 300],
-    duration: 500,
-    easing: "easeOutCubic",
-    delay: (events.value.length - el.dataset.index) * 50, // reverse our custom delay
-    complete: done,
-  });
+    anime({
+        targets: el,
+        opacity: [1, 0],
+        translateX: [0, -300],
+        duration: 500,
+        easing: "easeInCubic",
+        complete: done,
+    });
 };
 </script>
 
 <style lang="scss">
 .event-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-auto-flow: row; /* Ensure items are placed row by row */
-  padding: var(--space-lg);
-  gap: var(--space-lg);
-  position: relative;
-  overflow: hidden;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-flow: row;
+    padding: var(--space-lg);
+    gap: var(--space-lg);
+    position: relative;
+    overflow: hidden;
 }
 
 .empty {
-  margin: var(--space-xxl);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100dvh;
-  .text {
-    margin-bottom: var(--space-lg);
-    font-weight: 700;
-    font-size: var(--fs-lg);
-  }
+    margin: var(--space-xxl);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100dvh;
+    .text {
+        margin-bottom: var(--space-lg);
+        font-weight: 700;
+        font-size: var(--fs-lg);
+    }
 }
 </style>
